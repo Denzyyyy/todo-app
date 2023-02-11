20ae18afb468af54b19285ca44eaa890ff70ab93
izah <izahchidimma@gmail.com>
2023-02-10 19:00:52 -0600

    Initial commit

---
+++ experiments/bonus.py
@@ -0,0 +1,5 @@
+def solution(param1, param2):
+    int param1 = 1
+    int param2 = 2
+    param3 = param1 + param2
+    print(param3)
---
+++ experiments/e2.py
@@ -0,0 +1,3 @@
+while True:
+    with open("sides.txt", "r") as file:
+play = input("Throw the coin and enter head or tail here: ?")
